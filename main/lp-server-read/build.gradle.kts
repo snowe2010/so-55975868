c917f6fd1634e9270016c44d72e836eaf58d51cd
Tyler Thrailkill <tyler.thrailkill@promontech.com>
2019-05-16 13:11:29 -0600

    fix version

--- main/lp-server-read/build.gradle.kts
+++ main/lp-server-read/build.gradle.kts
@@ -3,6 +3,6 @@ dependencies {
     implementation(project(":lp-server-common"))
     implementation(project(":lp-server-loanapp-common"))
 
-    implementation("com.promontech.loanplatform:data-mapping:${extra["dataMappingVersion"]}")
+    implementation("com.promontech.loanplatform:data-mapping:0.6/0")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 }
